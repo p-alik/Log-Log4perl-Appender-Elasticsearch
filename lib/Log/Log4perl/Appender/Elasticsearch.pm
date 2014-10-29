ff7852db2742d3d080a5d7062cb1f1ac28db5805
Alexei Pastuchov <alexei.pastuchov@telecolumbus.de>
2014-10-29 11:25:52 +0100

    rm @ in pod

--- lib/Log/Log4perl/Appender/Elasticsearch.pm
+++ lib/Log/Log4perl/Appender/Elasticsearch.pm
@@ -291,7 +291,7 @@ L<https://github.com/p-alik/Log-Log4perl-Appender-Elasticsearch.git>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2014 by Alexei Pastuchov E<lt>palik@cpan.comE<gt>.
+Copyright 2014 by Alexei Pastuchov E<lt>palik at cpan.comE<gt>.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
